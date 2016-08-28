4df9c608eef30c20d6f0c7d7632f840359074056
Brian Huqueriza <bhuqueriza@gmail.com>
2016-08-28 00:51:11 -0700

    added angelo's new assets (tower, van, tickets); frame now 'respawns' at origin after hitting the floor

--- Assets/_Scripts/PictureFrame.cs
+++ Assets/_Scripts/PictureFrame.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class PictureFrame : MonoBehaviour {
 
+    public PictureFrame pictureFrame;
+    public Vector3 origin;
+
 	// Use this for initialization
-	void Start () {
-	
-	}
+	void Start ()
+    {
+        origin = transform.position;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	    
+	void Update ()
+    {
 
 	}
 
@@ -19,6 +23,7 @@ public class PictureFrame : MonoBehaviour {
         if (col.gameObject.tag == "Floor")
         {
             Destroy(gameObject);
+            PictureFrame newFrame = Instantiate(pictureFrame, origin, Quaternion.identity) as PictureFrame;
         }
     }
 }
